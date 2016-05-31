510da32a07b943dafe94e31833ad70bd4becf17f
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-31 17:36:10 +0200

    readibility

--- libdaemon.py
+++ libdaemon.py
@@ -18,9 +18,9 @@ class Daemon:
                stdin='/dev/null',
                stdout='/dev/null',
                stderr='/dev/null'):
-    self.stdin = stdin
-    self.stdout = stdout
-    self.stderr = stderr
+    self.stdin   = stdin
+    self.stdout  = stdout
+    self.stderr  = stderr
     self.pidfile = pidfile
 
   def daemonize(self):
