8d090abf86eab892bddef658f5029db9e522bf4a
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-02-04 16:17:57 +0100

    title updare

--- graph21.py
+++ graph21.py
@@ -49,7 +49,7 @@ def makegraph():
     ax4 = plt.subplot2grid((2, 3), (1, 2))
 
     plt.subplots_adjust(left=LMARG, bottom=None, right=RMARG, top=None,  wspace=0.01, hspace=None)
-    plt.suptitle(datetime.datetime.now())
+    plt.suptitle('Temperature DS18B20 ( ' + datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S") + ' )')
 
     # AX1 [YEAR]
     ax1.set_ylabel('Temperature [degC]')
