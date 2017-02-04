5acd00b6b3428cc50a5590f1e7c736e05b8f4e17
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-02-04 09:40:27 +0100

    resize figure

--- graph21.py
+++ graph21.py
@@ -40,11 +40,10 @@ def makegraph():
   # rule-of-thumb is to require more than 30 points available for the day-graph
   if len(DY) > 30:
     plt.figure(0)
-    ax1 = plt.subplot2grid((3,3), (0,0), colspan=3)
-    ax2 = plt.subplot2grid((3,3), (1,0), colspan=2)
-    ax3 = plt.subplot2grid((3,3), (1, 2), rowspan=2)
-    ax4 = plt.subplot2grid((3,3), (2, 0))
-    ax5 = plt.subplot2grid((3,3), (2, 1))
+    ax1 = plt.subplot2grid((2, 3), (0, 0), colspan=3)
+    ax2 = plt.subplot2grid((2, 3), (1, 0))
+    ax3 = plt.subplot2grid((2, 3), (1, 1))
+    ax4 = plt.subplot2grid((2, 3), (1, 2))
 
     plt.suptitle("subplot2grid")
 
