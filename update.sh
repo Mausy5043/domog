ae54d72c46f4b374feecdaaf7735f06987c44abe
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-02-04 09:09:00 +0100

    no sleep

--- update.sh
+++ update.sh
@@ -8,8 +8,8 @@ HOSTNAME=$(cat /etc/hostname)
 branch=$(cat "${HOME}/.domog.branch")
 
 # Wait for the daemons to finish their job. Prevents stale locks when restarting.
-echo "Waiting 30s..."
-sleep 30
+#echo "Waiting 30s..."
+#sleep 30
 
 # make sure working tree exists
 if [ ! -d /tmp/domog/site/img ]; then
