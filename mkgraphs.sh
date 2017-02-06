c1e4bf9ba7b5f330c38622a7a23d36d47f2708a8
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-02-06 18:00:14 +0100

    support renamed py-script

--- mkgraphs.sh
+++ mkgraphs.sh
@@ -8,20 +8,24 @@ UTC=$(date -u -d "$LOCAL" +"%Y-%m-%d %H:%M:%S")  #remove timezone reference
 UTCSECONDS=$(date -d "$UTC" +%s)
 UTCOFFSET=$((LOCALSECONDS - UTCSECONDS))
 
+./graph21.py
+./graph22.py
+./graph23.py
+
 pushd "$HOME/domog" >/dev/null
   if [ $(wc -l < /tmp/domog/mysql/sql21d.csv) -gt 30 ]; then
-    time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph21.gp
+    # time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph21.gp
   fi
   if [ $(wc -l < /tmp/domog/mysql/sql22d.csv) -gt 30 ]; then
-    time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph22.gp
+    # time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph22.gp
   fi
   if [ $(wc -l < /tmp/domog/mysql/sql23d.csv) -gt 30 ]; then
-    time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph23.gp
+    # time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph23.gp
   fi
 
   if [ $(wc -l < /tmp/domog/mysql/sql29.csv) -gt 30 ]; then
-    time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph29.gp
-    time ./graph29.py
+    # time gnuplot -e "utc_offset='${UTCOFFSET}'" ./graph29.gp
+    time ./graph29roos.py
     chmod 644 /tmp/domog/site/img/day29roos.png
     chmod 644 /tmp/domog/site/img/day29.png
   fi
