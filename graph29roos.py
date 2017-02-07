921cdf1c472719997477253cbcdae9d214f4baef
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-02-07 21:32:13 +0100

    size 320x320

--- graph29roos.py
+++ graph29roos.py
@@ -69,7 +69,7 @@ def makegraph29roos():
   plt.figure(0)
   fig = plt.gcf()
   DPI = fig.get_dpi()
-  fig.set_size_inches(640.0/float(DPI), 640.0/float(DPI))
+  fig.set_size_inches(320.0/float(DPI), 320.0/float(DPI))
 
   # bar plot on a polar axis.
   # number of datapoints to show
