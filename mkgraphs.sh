68ae3079a4ea85f56ad348c0b25eafa7f283df1a
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-02-11 17:05:59 +0100

    code cleanup

--- mkgraphs.sh
+++ mkgraphs.sh
@@ -2,12 +2,6 @@
 
 # Graph the data.
 
-LOCAL=$(date)
-LOCALSECONDS=$(date -d "$LOCAL" +%s)
-UTC=$(date -u -d "$LOCAL" +"%Y-%m-%d %H:%M:%S")  #remove timezone reference
-UTCSECONDS=$(date -d "$UTC" +%s)
-UTCOFFSET=$((LOCALSECONDS - UTCSECONDS))
-
 pushd "$HOME/domog" >/dev/null
   ./graph21.py
   ./graph22.py
