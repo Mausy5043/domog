28e6b451f0f006a5688a2a51485b25b56f64b024
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-02-19 09:51:16 +0100

    empty daemon

---
+++ again71d.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+# daemon71d.py creates a graph.
+
+import configparser
+import os
+import platform
+import shutil
+import sys
+import syslog
+import time
+import traceback
+
+from libdaemon import Daemon
+
+# constants
+DEBUG       = False
+IS_JOURNALD = os.path.isfile('/bin/journalctl')
+MYID        = "".join(list(filter(str.isdigit, os.path.realpath(__file__).split('/')[-1])))
+MYAPP       = os.path.realpath(__file__).split('/')[-2]
+NODE        = os.uname()[1]
+
+class MyDaemon(Daemon):
+  def run(self):
+    iniconf         = configparser.ConfigParser()
+    inisection      = MYID
+    home            = os.path.expanduser('~')
+    s               = iniconf.read(home + '/' + MYAPP + '/config.ini')
+    syslog_trace("Config file   : {0}".format(s), False, DEBUG)
+    syslog_trace("Options       : {0}".format(iniconf.items(inisection)), False, DEBUG)
+    reportTime      = iniconf.getint(inisection, "reporttime")
+    samplesperCycle = iniconf.getint(inisection, "samplespercycle")
+    flock           = iniconf.get(inisection, "lockfile")
+    sampleTime      = reportTime/samplesperCycle        # time [s] between samples
+
+    while True:
+      try:
+        startTime   = time.time()
+
+        do_main(flock)
+
+        waitTime    = sampleTime - (time.time() - startTime) - (startTime % sampleTime)
+        if (waitTime > 0):
+          syslog_trace("Waiting  : {0}s".format(waitTime), False, DEBUG)
+          syslog_trace("................................", False, DEBUG)
+          time.sleep(waitTime)
+      except Exception:  # Gotta catch em all
+        syslog_trace("Unexpected error in run()", syslog.LOG_CRIT, DEBUG)
+        syslog_trace(traceback.format_exc(), syslog.LOG_CRIT, DEBUG)
+        raise
+
+def do_main(flock):
+  lock(flock)
+  syslog_trace("* Main Loop Start", False, DEBUG)
+  # ff wachten
+  time.sleep(2)
+  unlock(flock)
+
+def lock(fname):
+  open(fname, 'a').close()
+
+def unlock(fname):
+  if os.path.isfile(fname):
+    os.remove(fname)
+
+def syslog_trace(trace, logerr, out2console):
+  # Log a python stack trace to syslog
+  log_lines = trace.split('\n')
+  for line in log_lines:
+    if line and logerr:
+      syslog.syslog(logerr, line)
+    if line and out2console:
+      print(line)
+
+
+if __name__ == "__main__":
+  daemon = MyDaemon('/tmp/' + MYAPP + '/' + MYID + '.pid')
+  if len(sys.argv) == 2:
+    if 'start' == sys.argv[1]:
+      daemon.start()
+    elif 'stop' == sys.argv[1]:
+      daemon.stop()
+    elif 'restart' == sys.argv[1]:
+      daemon.restart()
+    elif 'foreground' == sys.argv[1]:
+      # assist with debugging.
+      print("Debug-mode started. Use <Ctrl>+C to stop.")
+      DEBUG = True
+      syslog_trace("Daemon logging is ON", syslog.LOG_DEBUG, DEBUG)
+      daemon.run()
+    else:
+      print("Unknown command")
+      sys.exit(2)
+    sys.exit(0)
+  else:
+    print("usage: {0!s} start|stop|restart|foreground".format(sys.argv[0]))
+    sys.exit(2)
