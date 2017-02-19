b7522558e656aa84fd9e07095a0a5d98e135ca26
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-02-19 09:47:23 +0100

    Add graph from daemon 71

--- again82d.py
+++ again82d.py
@@ -62,6 +62,7 @@ def do_markdown(flock, fdata):
   shutil.copyfile(home + '/' + MYAPP + '/default.md', fdata)
 
   with open(fdata, 'a') as f:
+    f.write('![A GNUplot image should be here: day21.png](img/day71.png)\n')
     f.write('![A GNUplot image should be here: day21.png](img/day21.png?classes=zoomer)\n')
     f.write('![A GNUplot image should be here: day22.png](img/day22.png?classes=zoomer)\n')
     f.write('![A GNUplot image should be here: day23.png](img/day23.png?classes=zoomer)\n')
